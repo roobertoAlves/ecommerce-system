924d9194c0ba051f71bb0afd3f403dd1f1a35648
Roberto <robertosnpiii@gmail.com>
2026-07-29 15:24:20 -0300

    chore: run lint

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -95,11 +95,12 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
+    api.on("init", onSelect)
     api.on("reInit", onSelect)
     api.on("select", onSelect)
 
     return () => {
+      api?.off("init", onSelect)
       api?.off("select", onSelect)
     }
   }, [api, onSelect])
